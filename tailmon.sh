0099c59f9459d0483f234352463664ce1ded46d5
ViktorJp <97465574+ViktorJp@users.noreply.github.com>
2024-06-09 11:35:58 -0400

    Update tailmon.sh

--- tailmon.sh
+++ tailmon.sh
@@ -288,7 +288,7 @@ expressinstall()
         echo -e "${CGreen}Installing Tailscale Package(s)...${CClear}"
         echo ""
         archker=$(opkg print-architecture | grep "armv7-2.6")
-        if [ -z "archker" ]; then
+        if [ -z "$archker" ]; then
           opkg install tailscale
         else
           opkg install tailscale_nohf #install special tailscale package for arm7 kernel 2.6
