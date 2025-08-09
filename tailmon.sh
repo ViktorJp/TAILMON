c009de510f3bdcbd438cc96e00af01ff039b9f52
ViktorJp <97465574+ViktorJp@users.noreply.github.com>
2025-08-09 15:19:25 +0000

    Update tailmon.sh

--- tailmon.sh
+++ tailmon.sh
@@ -13,7 +13,7 @@
 export PATH="/sbin:/bin:/usr/sbin:/usr/bin:$PATH"
 
 #Static Variables - please do not change
-version="1.2.04b"
+version="1.2.0b4"
 beta=1
 apppath="/jffs/scripts/tailmon.sh"                                   # Static path to the app
 config="/jffs/addons/tailmon.d/tailmon.cfg"                          # Static path to the config file
